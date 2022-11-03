2e3bc6c40c5a02095b9c4f667aff2e176ea9c7a6
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2022-11-04 02:46:38 +0700

    user page load user data

--- pages/UserPage.tsx
+++ pages/UserPage.tsx
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View} from 'react-native';
 import {GoToInvoicePageButton} from '../components/GoToInvoiceButton';
 import {HeaderBanner} from '../components/HeaderBanner';
 import {LogoutButton} from '../components/LogoutButton';
+import {AuthService, IUserFull} from '../libs/auth.service';
 import {defaultStyles} from '../styles/default.style';
 
 export const UserPage = () => {
+  const [userData, setUserData] = useState<IUserFull | null>(null);
+
+  async function load() {
+    const res = await AuthService.getUser();
+    setUserData(res);
+  }
+
+  React.useEffect(() => {
+    load();
+  }, []);
+
+  if (!userData) {
+    return null;
+  }
+
   return (
     <>
       <View style={defaultStyles.main}>
-        <HeaderBanner headerText="Car" />
+        <HeaderBanner headerText={`Welcome, ${userData.user_firstname}`} />
 
-        <View style={defaultStyles.mt10}>
+        <View style={defaultStyles.mt05}>
           <GoToInvoicePageButton />
         </View>
-        <View style={defaultStyles.mt10}>
+        <View style={defaultStyles.mt05}>
           <LogoutButton />
         </View>
       </View>
